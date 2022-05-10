3cdd6ff04220dfad6471e1dcf74ad715cae4be10
Azoxlpb <azox.rekt@gmail.com>
2022-05-10 13:55:33 +0200

    Update datalist_gare.js

--- js/datalist_gare.js
+++ js/datalist_gare.js
@@ -22,24 +22,23 @@ list_gare();
 async function addStation(){
     let req = await (await fetch(`https://gigondas.iut-valence.fr:1112/sprietna/ihm/tp4/stations`)).json();
 	document.querySelector(`#search_depart_gare`).innerHTML = '';
-    let value =  document.querySelector(`search_depart`).value;
+    let value =  document.querySelector(`#dep`).value;
     console.log(value);
     for (let i = 0; i < req.length; i++) {
 		if (req[i].city == value) {
-			document.querySelector(`#search_depart`).innerHTML += `<option value="${req[i].name}"></option>`;
+			document.querySelector(`#search_depart_gare`).innerHTML += `<option value="${req[i].name}"></option>`;
+		}
+	}
+
+    let req2 = await (await fetch(`https://gigondas.iut-valence.fr:1112/sprietna/ihm/tp4/stations`)).json();
+	document.querySelector(`#search_depart_gare`).innerHTML = '';
+    let value2 =  document.querySelector(`#arr`).value;
+    for (let i = 0; i < req2.length; i++) {
+		if (req2[i].city == value2) {
+			document.querySelector(`#search_arrivee_gare`).innerHTML += `<option value="${req2[i].name}"></option>`;
 		}
 	}
 
 }
 
 addStation();
-/* 
-let req2 = await (await fetch(`https://gigondas.iut-valence.fr:1112/sprietna/ihm/tp4/stations`)).json();
-	document.querySelector(`#search_depart_gare`).innerHTML = '';
-    let value2 =  document.querySelector(`#search_depart`).value;
-    for (let i = 0; i < req2.length; i++) {
-		if (req2[i].city == value2) {
-			document.querySelector(`#search_depart`).innerHTML += `<option value="${req2[i].name}"></option>`;
-		}
-	}
-*/
