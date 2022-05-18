d864f6b0349ca656e99319dd4a5fde6a525abaa3
Azoxlpb <azox.rekt@gmail.com>
2022-05-18 11:22:34 +0200

    datalist fonctionnel

--- js/datalist_gare.js
+++ js/datalist_gare.js
@@ -1,23 +1,13 @@
-async function list_gare() {
-    const response = await fetch('http://gigondas:1111/sprietna/ihm/tp4/stations');
-    const names = await response.json();
-    for (let index = 0; index < names.length; index++) {
-        document.querySelector("#search_depart_gare").innerHTML += ` <option value="${names[index].name}">${names[index].name}</option>`;
-    }
-    for (let index = 0; index < names.length; index++) {
-        document.querySelector("#search_arrivee_gare").innerHTML += ` <option value="${names[index].name}">${names[index].name}</option>`;
-    }
-    
-    const response2 = await fetch('http://gigondas:1111/sprietna/ihm/tp4/cities');
-    const names2 = await response2.json();
-    for (let index = 0; index < names2.length; index++) {
-        document.querySelector("#search_depart").innerHTML += ` <option value="${names2[index].name}">${names2[index].name}</option>`;
-    }
-    for (let index = 0; index < names2.length; index++) {
-        document.querySelector("#search_arrivee").innerHTML += ` <option value="${names2[index].name}">${names2[index].name}</option>`;
+async function list_ville() {    
+    document.querySelector("#search_depart").innerHTML = ''
+    document.querySelector("#search_arrivee").innerHTML = ''
+    const response = await (await fetch('http://gigondas:1111/sprietna/ihm/tp4/cities')).json();
+    for (let index = 0; index < response.length; index++) {
+        document.querySelector("#search_depart").innerHTML += ` <option value="${response[index].name}">${response[index].name}</option>`;
+        document.querySelector("#search_arrivee").innerHTML += ` <option value="${response[index].name}">${response[index].name}</option>`;
     }
 }
-list_gare();
+list_ville();
 
 async function addStation(){
     let req = await (await fetch(`https://gigondas.iut-valence.fr:1112/sprietna/ihm/tp4/stations`)).json();
@@ -29,15 +19,13 @@ async function addStation(){
 		}
 	}
 
-    let req2 = await (await fetch(`https://gigondas.iut-valence.fr:1112/sprietna/ihm/tp4/stations`)).json();
-	document.querySelector(`#search_depart_gare`).innerHTML = '';
+	document.querySelector(`#search_arrivee_gare`).innerHTML = '';
     let value2 =  document.querySelector(`#arr`).value;
-    for (let i = 0; i < req2.length; i++) {
-		if (req2[i].city == value2) {
-			document.querySelector(`#search_arrivee_gare`).innerHTML += `<option value="${req2[i].name}"></option>`;
+    for (let i = 0; i < req.length; i++) {
+		if (req[i].city == value2) {
+			document.querySelector(`#search_arrivee_gare`).innerHTML += `<option value="${req[i].name}"></option>`;
 		}
 	}
 
 }
-
 addStation();
